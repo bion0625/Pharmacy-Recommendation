987bba3cbdd4be48d34d1759fc86ff06638aa2fc
bion0625 <bion0625@gmail.com>
2024-03-06 15:36:25 +0900

    약국 정보 redis에 cache로 저장 및 불러오는 로직 구현

--- src/main/java/com/example/project/pharmacy/service/PharmacySearchService.java
+++ src/main/java/com/example/project/pharmacy/service/PharmacySearchService.java
@@ -1,5 +1,6 @@
 package com.example.project.pharmacy.service;
 
+import com.example.project.pharmacy.cache.PharmacyRedisTemplateService;
 import com.example.project.pharmacy.dto.PharmacyDto;
 import com.example.project.pharmacy.entity.Pharmacy;
 import lombok.RequiredArgsConstructor;
@@ -14,11 +15,14 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class PharmacySearchService {
 
-    private PharmacyRepositoryService pharmacyRepositoryService;
+    private final PharmacyRepositoryService pharmacyRepositoryService;
+    private final PharmacyRedisTemplateService pharmacyRedisTemplateService;
 
     public List<PharmacyDto> searchPharmacyDtoList() {
 
         // redis
+        List<PharmacyDto> pharmacyDtoList = pharmacyRedisTemplateService.findAll();
+        if (!pharmacyDtoList.isEmpty()) return pharmacyDtoList;
 
         // db
         return pharmacyRepositoryService.findAll()
